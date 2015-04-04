e10804bc1b28b079e1197e4b194b236531ab3b84
KrivenkoAlexander <krivenkotester@gmail.com>
2015-04-04 21:07:55 +0300

    Параметризация для контактов и групп
    -есть проблемы с пробелами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
+import string
+import random
+import pytest
 
-def test_test_add_contact(app):
-    app.open_home_page()
-    old_list=app.contact.get_list()
-    contact= Contact(firstname="Alex", middlename="a", lastname="Kriv", nickname="sd", title="fdf",
-                                        company="sdd", address="we", homephone="we", mobilephone="433", workphone="443", fax="3",
+def random_str(prefix,maxlen):
+    symbols= string.ascii_letters + string.digits
+    return prefix+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+test_data=[Contact(firstname=random_str("firstname",10), middlename=random_str("middlename",10), lastname=random_str("lastname",10), nickname='nickname', title='title',
+                                        company="sdd", address="we", homephone="500", mobilephone="433", workphone="443", fax="3",
                                         byear="1988", ayear="1988",address2="retert", phone2="rr", notes="e",
                                         droplist="//div[@id='content']/form/select[1]//option[4]",
                                         droplist2="//div[@id='content']/form/select[2]//option[8]",
-                                        droplist3="//div[@id='content']/form/select[3]//option[3]",droplist4="//div[@id='content']/form/select[4]//option[8]")
-    app.contact.create_contact(contact)
-    new_list=app.contact.get_list()
-    assert len(old_list)+1 == len(new_list)
-    old_list.append(contact)
-    assert sorted(old_list,key=Contact.id_or_max) == sorted(new_list,key=Contact.id_or_max)
+                                        droplist3="//div[@id='content']/form/select[3]//option[3]",droplist4="//div[@id='content']/form/select[4]//option[8]") for i in range(3)]
 
-#def test_test_add_empty_contact(app):
-#    app.open_home_page()
-#    old_list=app.contact.get_list()
-#    app.contact.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-#                                        address="", home="", mobile="", work="", fax="", byear="", ayear="",
-#                                        address2="", phone2="", notes="",
-#                                        droplist="//div[@id='content']/form/select[1]//option[4]",
-#                                        droplist2="//div[@id='content']/form/select[2]//option[8]",
-#                                        droplist3="//div[@id='content']/form/select[3]//option[3]",droplist4="//div[@id='content']/form/select[4]//option[8]"))
-#    new_list=app.contact.get_list()
-#    assert len(old_list)+1 == len(new_list)
+@pytest.mark.parametrize('contact',test_data,ids=[repr(x) for x in test_data] )
+def test_test_add_contact(app,contact):
+     app.open_home_page()
+     old_list=app.contact.get_list()
+     app.contact.create_contact(contact)
+     new_list=app.contact.get_list()
+     assert len(old_list)+1 == len(new_list)
+     old_list.append(contact)
+     assert sorted(old_list,key=Contact.id_or_max) == sorted(new_list,key=Contact.id_or_max)
